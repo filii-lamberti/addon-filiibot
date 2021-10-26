e5e61de19667ad1d29a7aa58f8db23f9df190f44
Jorim Tielemans <tielemans.jorim@gmail.com>
2021-10-26 13:06:01 +0200

    test

--- filiibot/rootfs/opt/filiibot/commands/kick.js
+++ filiibot/rootfs/opt/filiibot/commands/kick.js
@@ -1,5 +1,12 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
 module.exports = {
-  name: 'kick',
-  description: 'Kick a user from the server.',
-  guildOnly: true,
+	data: new SlashCommandBuilder()
+		.setName('kick')
+		.setDescription('Select a member and kick them (but not really).')
+		.addUserOption(option => option.setName('target').setDescription('The member to kick')),
+	async execute(interaction) {
+		const user = interaction.options.getUser('target');
+		return interaction.reply({ content: `You wanted to kick: ${user.username}`, ephemeral: true });
+	},
 };
--- filiibot/rootfs/opt/filiibot/commands/ping.js
+++ filiibot/rootfs/opt/filiibot/commands/ping.js
@@ -1,18 +1,10 @@
-/*
- * If the command is 'ping'
- * Calculates ping between sending a message and editing it, giving a nice round-trip latency.
- * The second ping is an average latency between the bot and the websocket server
- * (one-way, not round-trip)
- */
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
 module.exports = {
-  name: 'ping',
-  cooldown: 5,
-  description: 'Ping!',
-  async execute(message, _args) {
-    const m = await message.channel.send('Ping?');
-    m.edit(
-      `Pong! Wachttijd is ${m.createdTimestamp - message.createdTimestamp}ms,
-      API wachttijd is ${Math.round(message.client.ws.ping)}ms.`,
-    );
-  },
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		return interaction.reply('Pong!');
+	},
 };
